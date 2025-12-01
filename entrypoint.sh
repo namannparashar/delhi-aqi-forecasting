6df09d2824ef8af19797fd5134bb1ccf511c6014
Naman <namann.parashar@gmail.com>
2025-12-01 03:31:11 +0000

    Added a timer to give enough time to API

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 python -m uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
+echo "Waiting for API to start..."
+sleep 15
+
 streamlit run dashboard/streamlit_app.py --server.port 7860 --server.address 0.0.0.0 
